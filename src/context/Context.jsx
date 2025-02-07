1853ceef6e195d81040584ad756ccf63ae47e1d4
Students-g16 <gruppag783@gmail.com>
2025-02-07 18:36:10 +0500

    like funksiyasi amalga oshirildi

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,12 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+// ShopContext nomiga createContext()-dan foydalanib context yaratildi
 export const ShopContext = createContext();
 
+// ShopProvider funksional component
 export const ShopProvider = ({ children }) => {
+  // navigate nomli o'zgaruvchi e'lon qilinib unga useNavigate hookini vazifasi yuklatildi
   const navigate = useNavigate();
+  // useState() componentlar holatini boshqaradi
+  // likedProducts bu mahsulotlarni yoqtirilganlik state(holat), setLikedProducts bu setState funksiyasi bolib state-ni boshqaradi , useState(initialState => boshlang'ich holat)
+  const [likedProducts, setLikedProducts] = useState([]);
+
+  // toggleLike funksiyasi mahsulotlarga like bosish holatini bajaradi
+  // parametr bu funksiyaga qiymat kiritish hisoblanadi
+  // includes() bu array(massiv) metodi bolib ,uning vazifasi qiymat bor yoki yoqligini tekshiradi (true/false)
+  // filter() bu metod kiritilgan shartga mos kelgan qiymatni qaytaradi
+  // ... bu spread operatori deyiladi va bu malumotlarni yoyib olishda yordam beradi
+  const toggleLike = (productId) => {
+    setLikedProducts((likedList) =>
+      likedList.includes(productId)
+        ? likedList.filter((id) => id !== productId)
+        : [...likedList, productId]
+    );
+  };
 
   return (
-    <ShopContext.Provider value={{ navigate }}>{children}</ShopContext.Provider>
+    // Provider (yetkazib beruvchi) bu malumotlarni componentlarga yetkazib beruvchi hisoblanadi
+    <ShopContext.Provider value={{ navigate, likedProducts, toggleLike }}>
+      {children}
+    </ShopContext.Provider>
   );
 };
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { products } from "../assets/data";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { GrShop } from "react-icons/gr";
+import { ShopContext } from "../context/Context";
 
 export default function Products() {
+  // useContext hooki yaratilgan context-dan uzatilgan malumotlarni qabul qiladi
+  const { likedProducts, toggleLike } = useContext(ShopContext);
   return (
     <main>
       <section className="h-screen max-w-screen-lg mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center gap-10 my-10">
@@ -12,17 +15,31 @@ export default function Products() {
             key={mahsulot.id}
             className="relative flex flex-col items-center shadow-lg rounded-md p-10"
           >
-            <span className="absolute left-0 top-0 px-5 py-1 bg-red-500 text-white font-asul font-semibold rounded-md">{mahsulot.skidka}</span>
+            <span className="absolute left-0 top-0 px-5 py-1 bg-red-500 text-white font-asul font-semibold rounded-md">
+              {mahsulot.skidka}
+            </span>
             <img src={mahsulot.img} />
             <div className="w-full flex flex-col items-center">
-              <h1 className="font-asul font-bold text-xl text-green-700">{mahsulot.title}</h1>
+              <h1 className="font-asul font-bold text-xl text-green-700">
+                {mahsulot.title}
+              </h1>
               <div className="flex items-center gap-x-5">
-                <span className="line-through font-asul font-bold text-red-500">{mahsulot.oldPrice}</span>
-                <span className="font-asul font-bold text-green-700">{mahsulot.newPrice}</span>
+                <span className="line-through font-asul font-bold text-red-500">
+                  {mahsulot.oldPrice}
+                </span>
+                <span className="font-asul font-bold text-green-700">
+                  {mahsulot.newPrice}
+                </span>
               </div>
             </div>
             <div className="flex items-center justify-between w-full">
-              <FaRegHeart />
+              <button onClick={() => toggleLike(mahsulot.id)}>
+                {likedProducts.includes(mahsulot.id) ? (
+                  <FaHeart className="text-red-500" />
+                ) : (
+                  <FaRegHeart />
+                )}
+              </button>
               <GrShop />
             </div>
           </div>
